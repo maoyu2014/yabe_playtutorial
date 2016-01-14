30bc42c230057d56a51d17172469852a9b28db16
maoyu2014 <haczmy@qq.com>
2016-01-14 15:45:37 +0800

    Adding authentication

--- app/controllers/Comments.java
+++ app/controllers/Comments.java
@@ -2,6 +2,8 @@ package controllers;
 
 import play.mvc.*;
 
+@Check("admin")
+@With(Secure.class)
 public class Comments extends CRUD {
 
 
--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -2,6 +2,8 @@ package controllers;
 
 import play.mvc.*;
 
+@Check("admin")
+@With(Secure.class)
 public class Users extends CRUD {
 
 
