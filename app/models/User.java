9f970b29f100f3c61af7645a41f3fb9c6aabda0b
maoyu2014 <haczmy@qq.com>
2016-01-14 10:05:15 +0800

    A basic admin area using CRUD

--- app/models/User.java
+++ app/models/User.java
@@ -2,13 +2,20 @@ package models;
 
 import javax.persistence.Entity;
 
+import play.data.validation.Email;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
 public class User extends Model {
 
+	@Email
+    @Required
 	public String email;
+	
+	@Required
 	public String password;
+	
 	public String fullname;
 	public boolean isAdmin;
 	
@@ -20,6 +27,10 @@ public class User extends Model {
 	
 	public static User connect(String email, String password) {
 		return find("byEmailAndPassword",email,password).first();
+	}
+	
+	public String toString() {
+	    return email;
 	}
 	
 }
